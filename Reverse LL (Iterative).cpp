8cb55dae4b107ab06706a4da13d2f11aaa121fa0
mayankwatts <47928731+mayankwatts@users.noreply.github.com>
2019-05-11 09:54:24 +0530

    Create Reverse LL (Iterative).cpp

---
+++ Reverse LL (Iterative).cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+class node{
+public:
+    int data;
+    node * next;
+    node(int data){
+        this->data=data;
+        this->next=NULL;
+    }
+};
+using namespace std;
+#include "solution.h"
+node* takeinput(){
+    int data;
+    cin>>data;
+    node* head=NULL,*tail=NULL;
+    while(data!=-1){
+        node *newnode=new node(data);
+        if(head==NULL)                  {
+            head=newnode;
+            tail=newnode;
+        }
+        else{
+            tail->next=newnode;
+            tail=newnode;
+        }
+        cin>>data;
+    }
+    return head;
+}
+void print(node *head)
+{
+    node*temp=head;
+    while(temp!=NULL)
+    {
+        cout<<temp->data<<" ";
+        temp=temp->next;
+    }
+    cout<<endl;
+}
+
+node* rev_linkedlist_itr(node* head)
+{
+    node* prev=0;
+    node* temp=head;
+    node* next;
+    
+    while(temp!=0){
+        next=temp->next;
+        temp->next=prev;
+        prev=temp;
+        temp=next;
+        
+    }
+    
+    return prev;
+}
+
+int main(){
+    node* head=takeinput();
+    head=rev_linkedlist_itr(head);
+    print(head);
+    
+    return 0;
+}
