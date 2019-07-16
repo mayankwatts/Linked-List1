bc6f6ea1031145812b82f02fbdb6ed8d383395c8
mayankwatts <47928731+mayankwatts@users.noreply.github.com>
2019-07-16 16:13:19 +0530

    Create Print ith node.cpp

---
+++ Print ith node.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+class Node{
+public:
+    int data;
+    Node *next;
+    Node(int data){
+        this -> data = data;
+        this -> next = NULL;
+    }
+};
+
+using namespace std;
+
+void printIthNode(Node *head, int i) {
+    
+    while(i && head!=NULL){
+        head=head->next;
+        i--;
+    }
+    
+    if(head!=NULL)
+    cout<<head->data;
+    
+}
+
+Node* takeinput() {
+    int data;
+    cin >> data;
+    Node* head = NULL, *tail = NULL;
+    while(data != -1){
+        Node *newNode = new Node(data);
+        if(head == NULL)                  {
+            head = newNode;
+            tail = newNode;
+        }
+        else{
+            tail -> next = newNode;
+            tail = newNode;
+        }
+        cin >> data;
+    }
+    return head;
+}
+
+void print(Node *head) {
+    Node *temp = head;
+    while(temp != NULL) {
+        cout << temp -> data << " ";
+        temp = temp -> next;
+    }
+    cout<<endl;
+}
+
+int main() {
+    Node *head = takeinput();
+    int pos;
+    cin >> pos;
+    printIthNode(head, pos);
+    
+    return 0;
+}
